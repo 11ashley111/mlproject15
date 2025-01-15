d23ce4d90e1268722bb4b38d01c0e2dc5f72faaa
“ASHLEY” <ashlyyy6969@gmail.com>
2025-01-15 23:51:41 +0530

    data ingestion

---
+++ app.py
@@ -0,0 +1,20 @@
+from src.mlproject.logger import logging 
+from src.mlproject.exception import CustomException
+from src.mlproject.components.data_ingestion import DataIngestion
+from src.mlproject.components.data_ingestion import DataIngestionConfig
+
+import sys
+
+if __name__=="__main__":
+    logging.info("the execution is start")
+    
+    try:
+        
+        data_ingestion=DataIngestion()
+        
+        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+        
+    except Exception as e:
+        logging.info("custom exception")
+        raise CustomException(e,sys)
+    
